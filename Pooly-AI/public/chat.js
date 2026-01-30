75a6856f31083bd30c3a2b93ad0faa2834d0cf80
pooly <pooly.s_mood@outlook.com>
2026-01-30 12:36:10 +0100

    #

--- Pooly-AI/public/chat.js
+++ Pooly-AI/public/chat.js
@@ -18,11 +18,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // 2️⃣ SHADOW ROOT
   const shadow = host.attachShadow({ mode: "open" });
-  if (document.querySelector('main').classList.contains('hidden')) {
-    pallino.style.display = 'none';
-} else {
-    pallino.style.display = 'block';
-}
+
 
   // ===============================
   // 3️⃣ CSS (ex style.css AI)
@@ -30,6 +26,11 @@ window.addEventListener("DOMContentLoaded", () => {
   const POOLY_CSS = `
     /* PoolyAI Chat Style */
   `;
+  if (document.querySelector('main').classList.contains('hidden')) {
+    pallino.style.display = 'none';
+} else {
+    pallino.style.display = 'block';
+}
   const style = document.createElement("style");
   style.textContent = `
     * { box-sizing: border-box; }
