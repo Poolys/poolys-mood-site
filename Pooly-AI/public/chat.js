f4b264f0ddc82adc80383360e394aba366aa009d
pooly <pooly.s_mood@outlook.com>
2026-01-31 18:28:14 +0100

    #

--- Pooly-AI/public/chat.js
+++ Pooly-AI/public/chat.js
@@ -205,8 +205,8 @@ pallino.addEventListener("click", (e) => {
 
 // Questo ora funzionerà solo quando clicchi realmente fuori
 window.addEventListener("click", (e) => {
-    if (chatBody.classList.contains("open") && !chatBody.contains(e.target)) {
-        chatBody.classList.remove("open");
+    if (chat.classList.contains("open") && !chatBody.contains(e.target)) {
+        chat.classList.remove("open");
         pallino.classList.remove("closed");
     }
 });
