75022451fec489f95918882fb50e08eca32e013f
pooly <pooly.s_mood@outlook.com>
2026-03-15 21:26:22 +0100

    #

--- js/main.js
+++ js/main.js
@@ -48,8 +48,8 @@ window.dispatchEvent(event);
 
 
   document.body.classList.remove('locked');
-  document.body.style.overflow = 'hidden';
-  document.documentElement.style.overflow = 'hidden';
+  document.body.style.overflow = 'auto';
+  document.documentElement.style.overflow = 'auto';
 
   window.scrollTo(0, 0);
 }
